53411077eaf36947ffdac80115e6be1bca6c4a56
eevan7a9 <e7a9@protonmail.com>
2020-11-04 02:01:26 +0900

    created MyPage Greeting banner

---
+++ src/components/MyPageComponents/GreetingBanner.vue
@@ -0,0 +1,20 @@
+<template>
+  <q-card-section class="greeting-banner notosanskr-regular">
+    <div>고객님</div>
+    <div>안녕하세요 !</div>
+  </q-card-section>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+.greeting-banner {
+  padding-top: 28px;
+  font-size: 24px;
+  line-height: 36px;
+  letter-spacing: -1.8px;
+  color: #1a1a1a;
+}
+</style>
