5aa425d1e94fbb75ad140b0b53844a043ea84c3a
eevan7a9 <e7a9@protonmail.com>
2020-11-04 01:42:24 +0900

    created Toolbar for Mypage

---
+++ src/components/MyPageComponents/Toolbar.vue
@@ -0,0 +1,22 @@
+<template>
+  <q-card-section class="toolbar notosanskr-medium q-pa-none q-px-md">
+    <q-icon name="keyboard_backspace" size="24px"></q-icon>
+    <q-btn flat class="bg-primary text-white">로그인</q-btn>
+  </q-card-section>
+</template>
+
+<script>
+export default {
+
+};
+</script>
+
+<style scoped>
+.toolbar {
+  height: 44px;
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+</style>
