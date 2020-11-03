3f0dd954537b14e9e4ee3ea32e7bb47d2d9ede56
eevan7a9 <e7a9@protonmail.com>
2020-11-04 02:01:44 +0900

    added toolbar & greeting banner

--- src/pages/MyPage/Index.vue
+++ src/pages/MyPage/Index.vue
@@ -1,9 +1,25 @@
 <template>
-  <div style="min-height: 100vh" class="bg-positive"></div>
+  <div style="min-height: 100vh" class="bg-positive">
+    <q-card flat square class="row items-center">
+      <toolbar></toolbar>
+      <greeting-banner></greeting-banner>
+    </q-card>
+  </div>
 </template>
 
 <script>
-export default {};
+import { Toolbar, GreetingBanner } from "src/components/MyPageComponents";
+export default {
+  name: "MyPage",
+  components: {
+    toolbar: Toolbar,
+    "greeting-banner": GreetingBanner
+  }
+};
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.header {
+  border-radius: 0;
+}
+</style>
