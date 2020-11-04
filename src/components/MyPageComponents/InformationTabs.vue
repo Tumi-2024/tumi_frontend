3d58d2c0893869b76cf0c718695a77c3eef0d9dd
eevan7a9 <e7a9@protonmail.com>
2020-11-04 16:57:07 +0900

    added Information tabs

---
+++ src/components/MyPageComponents/InformationTabs.vue
@@ -0,0 +1,45 @@
+<template>
+  <q-card-section class="q-pa-none full-width" style="padding: 30px 16px;">
+    <div class="item row full-width" v-for="(tab, i) in tabs" :key="i">
+      <div class="row">
+        <div class="text col-12 notosanskr-medium">{{ tab }}</div>
+        <span class="highlighter full-width q-py-xs"></span>
+      </div>
+    </div>
+  </q-card-section>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      tabs: [
+        "실거래가 조회",
+        "재개발 구역 정보",
+        "아파트 매물 찾기",
+        "투자 매물 찾기",
+        "부동산 인사이트"
+      ]
+    };
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.item {
+  font-weight: 500;
+  font-size: 16px;
+  line-height: 24px;
+  letter-spacing: -1.2px;
+  color: #1a1a1a;
+  padding: 14px 0;
+}
+.text {
+  z-index: 1;
+}
+.highlighter {
+  background: #ffd8cc;
+  margin-top: -10px;
+  z-index: 0;
+}
+</style>
--- src/components/MyPageComponents/index.js
+++ src/components/MyPageComponents/index.js
@@ -1,5 +1,6 @@
 import Toolbar from "./Toolbar";
 import GreetingBanner from "./GreetingBanner";
 import IconTabs from "./IconTabs";
+import InformationTabs from "./InformationTabs";
 
-export { Toolbar, GreetingBanner, IconTabs };
+export { Toolbar, GreetingBanner, IconTabs, InformationTabs };
