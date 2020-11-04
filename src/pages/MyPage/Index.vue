4318313e8d35e297a0b0df8126ac8f9cd2e6644a
eevan7a9 <e7a9@protonmail.com>
2020-11-04 16:57:30 +0900

    added Information tabs & Notice

--- src/pages/MyPage/Index.vue
+++ src/pages/MyPage/Index.vue
@@ -1,9 +1,20 @@
 <template>
-  <div style="min-height: 100vh" class="bg-positive">
+  <div style="min-height: 100vh" class="row column bg-positive">
+    <!-- Head Section -->
     <q-card flat square class="row items-center">
       <toolbar></toolbar>
       <greeting-banner></greeting-banner>
       <icon-tabs></icon-tabs>
+    </q-card>
+    <!-- Information lists -->
+    <q-card flat square class="row items-center q-mt-sm">
+      <information-tabs></information-tabs>
+    </q-card>
+    <!-- Notice Section -->
+    <q-card flat square class="q-mt-sm full-heigh col-grow">
+      <q-card-section class="notice notosanskr-regular"
+        >공지사항</q-card-section
+      >
     </q-card>
   </div>
 </template>
@@ -12,20 +23,26 @@
 import {
   Toolbar,
   GreetingBanner,
-  IconTabs
+  IconTabs,
+  InformationTabs
 } from "src/components/MyPageComponents";
 export default {
   name: "MyPage",
   components: {
     toolbar: Toolbar,
     "greeting-banner": GreetingBanner,
-    "icon-tabs": IconTabs
+    "icon-tabs": IconTabs,
+    "information-tabs": InformationTabs
   }
 };
 </script>
 
 <style lang="scss" scoped>
-.header {
-  border-radius: 0;
+.notice {
+  font-weight: 500;
+  font-size: 14px;
+  line-height: 24px;
+  letter-spacing: -0.98px;
+  color: #707070;
 }
 </style>
