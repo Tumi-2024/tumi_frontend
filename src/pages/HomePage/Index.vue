e560293db5734aed6ead3145e11df4b91e5a933c
Reichard Cris <reichard@jdlife.co.nz>
2020-11-04 16:58:28 +0800

    Update HomePage/Index.vue

--- src/pages/HomePage/Index.vue
+++ src/pages/HomePage/Index.vue
@@ -4,7 +4,7 @@
       <home-search-bar key="2"/>
     </div>
     <div class="col-12 q-pl-lg q-pr-lg bg-white">
-      <home-dash-button />
+      <home-dashboard-buttons />
     </div>
     <div class="col-12 q-pl-lg q-pr-lg bg-white" style="margin-top: 12px">
       <home-investment-buttons />
@@ -13,13 +13,13 @@
 </template>
 
 <script>
-import { HomeSearchBar, HomeDashButton, HomeInvestmentButtons } from 'components/HomeComponents';
+import { HomeSearchBar, HomeInvestmentButtons, HomeDashboardButtons } from 'components/HomeComponents';
 
 export default {
   components: {
     HomeSearchBar,
-    HomeDashButton,
-    HomeInvestmentButtons
+    HomeInvestmentButtons,
+    HomeDashboardButtons
   }
 }
 </script>
