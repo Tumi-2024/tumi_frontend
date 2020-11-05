cabb8ac7b20e81456e1ac41fb924eb9d9edebe96
Jepril Achapero <jeprilachapero@ocomsoft.com>
2020-11-05 23:29:16 +0800

    developing property-interest

--- src/pages/PropertyInterestPage/Index.vue
+++ src/pages/PropertyInterestPage/Index.vue
@@ -1,6 +1,39 @@
 <template>
-  <div>
-    Property(Interest)
+  <div style="min-height: 100vh" class="row column bg-positive">
+    <div style="height: 48px; margin-top: 12px; background: #FFF;"  class="row">
+      <span style="padding-left: 16px; padding-right: 16px; font-size: 16px; line-height: 48px; font-weight: 500; font-style: normal;">
+        관심매물 5개
+      </span>
+    </div>
+
+    <div style="height: 48px; background: #F6F6F6;"  class="row">
+      <div class="col col-md-6">
+        <div class="row">
+          <div class="col col-md-3" style="text-align: center; ">
+            <a href="" style="line-height: 44px; font-size: 14px; font-weight: 500;  color: #D32C00; width: 100%; text-decoration: none;">최신순</a>
+          </div>
+
+          <div class="col col-md-3" style="text-align: center; ">
+            <a href="" style="line-height: 44px; font-size: 14px; font-weight: 500;  color: #707070; width: 100%; text-decoration: none;">추천순</a>
+          </div>
+
+          <div class="col col-md-3" style="text-align: center; ">
+            <a href="" style="line-height: 44px; font-size: 14px; font-weight: 500;  color: #707070; width: 100%; text-decoration: none;">면적순</a>
+          </div>
+          
+          <div class="col col-md-3" style="text-align: center;">
+            <a href="" style="line-height: 44px; font-size: 14px; font-weight: 500;  color: #707070; text-decoration: none;">가격순</a>
+          </div>
+        </div>
+      </div>
+
+      <div class="col col-md-6">
+        asd
+      </div>
+    </div>
+    <div style="height: 68px; background: #FFF;"  class="row">
+      
+    </div>
   </div>
 </template>
 
