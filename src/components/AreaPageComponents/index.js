ab645e737ea7143c854c962dfb6bcf221e5147cb
eevan7a9 <e7a9@protonmail.com>
2020-11-08 07:29:50 +0900

    Added Area filter btns section

--- src/components/AreaPageComponents/index.js
+++ src/components/AreaPageComponents/index.js
@@ -1,3 +1,4 @@
 import AreaCarousel from "./AreaCarousel";
+import AreaFilterBtns from "./AreaFilterBtns";
 
-export { AreaCarousel };
+export { AreaCarousel, AreaFilterBtns };
--- src/pages/AreaPage/Index.vue
+++ src/pages/AreaPage/Index.vue
@@ -1,13 +1,20 @@
 <template>
-  <div class="q-mt-sm"><area-carousel></area-carousel></div>
+  <div class="q-mt-sm">
+    <area-carousel></area-carousel>
+    <area-filter-btns></area-filter-btns>
+  </div>
 </template>
 
 <script>
-import { AreaCarousel } from "src/components/AreaPageComponents";
+import {
+  AreaCarousel,
+  AreaFilterBtns
+} from "src/components/AreaPageComponents";
 export default {
   name: "Area",
   components: {
-    "area-carousel": AreaCarousel
+    "area-carousel": AreaCarousel,
+    "area-filter-btns": AreaFilterBtns
   },
   data() {
     return {};
