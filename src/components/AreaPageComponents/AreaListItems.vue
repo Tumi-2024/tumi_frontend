3bbe37b1614e3e1d8de9fca0c819565c8333e88e
eevan7a9 <e7a9@protonmail.com>
2020-11-09 06:15:02 +0900

    added Area Items

--- src/components/AreaPageComponents/AreaListItems.vue
+++ src/components/AreaPageComponents/AreaListItems.vue
@@ -14,16 +14,75 @@
       <q-separator vertical />
       <q-btn flat>가격순</q-btn>
     </q-card-section>
-    <q-card-section class="list-items">
-      <q-item>
-        items
-      </q-item>
+    <q-card-section class="list-items q-pa-none">
+      <q-list class="q-pt-md">
+        <area-item
+          v-for="(item, i) of items"
+          :key="i"
+          :name="item.name"
+          :amount="item.amount"
+          :tags="item.tags"
+        ></area-item>
+      </q-list>
     </q-card-section>
   </q-card>
 </template>
 
 <script>
-export default {};
+import AreaItem from "./AreaItem";
+export default {
+  components: {
+    "area-item": AreaItem
+  },
+  data() {
+    return {
+      items: [
+        {
+          tags: {
+            type: "아파트",
+            recomend: "투미추천 급매",
+            date: "20.10.12"
+          },
+          name: "종로1가 대성스카이렉스",
+          amount: "보증금 5천 6백만 / 월세 120만"
+        },
+        {
+          tags: {
+            type: "아파트",
+            date: "20.10.12"
+          },
+          name: "종로1가 대성스카이렉스",
+          amount: "매매 6억 5,500만"
+        },
+        {
+          tags: {
+            type: "아파트",
+            redevelopment: true,
+            date: "20.10.12"
+          },
+          name: "종로1가 대성스카이렉스",
+          amount: "매매 6억 5천만"
+        },
+        {
+          tags: {
+            type: "아파트",
+            date: "20.10.12"
+          },
+          name: "종로1가 대성스카이렉스",
+          amount: "매매 6억 5,500만"
+        },
+        {
+          tags: {
+            type: "아파트",
+            date: "20.10.12"
+          },
+          name: "종로1가 대성스카이렉스",
+          amount: "매매 6억 5,500만"
+        }
+      ]
+    };
+  }
+};
 </script>
 
 <style lang="scss" scoped>
