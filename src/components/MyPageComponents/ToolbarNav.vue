4493efcdb12a835596d56a33f8239f8fbf3fc7d2
eevan7a9 <e7a9@protonmail.com>
2020-11-11 08:16:26 +0900

    updated back icon btn

--- src/components/MyPageComponents/ToolbarNav.vue
+++ src/components/MyPageComponents/ToolbarNav.vue
@@ -1,23 +1,28 @@
 <template>
   <q-card-section class="toolbar notosanskr-medium q-pa-none q-px-md">
-    <div>
-      <q-icon name="keyboard_backspace" size="24px"></q-icon>
+    <div class="row items-center">
+      <q-btn flat padding="0px" :to="{ name: 'my_page' }">
+        <q-icon name="keyboard_backspace" size="24px" class="q-mr-sm"></q-icon>
+      </q-btn>
+      <q-separator vertical inset></q-separator>
       <q-btn flat>내 관심 매물</q-btn>
     </div>
     <q-btn flat class="bg-white add-area" v-if="$route.name == 'area'">
       <q-icon size="24px">
         <img src="~assets/icons/add-area.svg" alt="" />
-        
       </q-icon>
       로그인
     </q-btn>
 
-    <q-btn flat class="bg-white add-area" v-if="$route.name == 'property_interest'">
+    <q-btn
+      flat
+      class="bg-white add-area"
+      v-if="$route.name == 'property_interest'"
+    >
       <q-icon size="24px">
         <img src="~assets/icons/Regular.svg" alt="" />
       </q-icon>
     </q-btn>
-    
   </q-card-section>
 </template>
 
