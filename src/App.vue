5ce0dde2220aed3672672dc9183d30087feffb80
eevan7a9 <e7a9@protonmail.com>
2020-11-11 22:23:09 +0900

    added Modal Login

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,16 @@
   <div id="q-app" class="bg-positive">
     <div style="max-width: 1000px; min-height: 100vh" class="q-mx-auto bg-white">
       <router-view />
+      <login-modal />
     </div>
   </div>
 </template>
 <script>
+import LoginModal from "src/components/Utilities/LoginModal/LoginModal.vue";
 export default {
-  name: 'App'
+  name: 'App',
+  components: {
+    "login-modal": LoginModal
+  }
 }
 </script>
