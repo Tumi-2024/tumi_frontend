528285baaa55b86b9c104770d4d21d6f566dca73
eevan7a9 <e7a9@protonmail.com>
2020-11-14 05:15:59 +0900

    set filter btns scrollable

--- src/components/AreaPageComponents/AreaFilterBtns.vue
+++ src/components/AreaPageComponents/AreaFilterBtns.vue
@@ -1,21 +1,23 @@
 <template>
   <q-card flat>
-    <q-card-section class="row justify-between">
+    <q-card-section class="row justify-between no-wrap">
       <q-btn
         unelevated
         outline
         color="primary"
         padding="8px"
         style="border-radius: 8px;"
+        class="q-mr-sm"
       >
         <q-icon>
           <img src="~assets/icons/Filter.svg" />
         </q-icon>
       </q-btn>
 
-      <div class=" notosanskr-medium">
-        <q-btn unelevated class="q-mr-sm">매매,전세,월세</q-btn>
-        <q-btn unelevated>아파트,오피스텔,재개발...</q-btn>
+      <div class="scrolling-wrapper-flexbox notosanskr-medium row">
+        <q-btn unelevated class="q-mr-xs">매매,전세,월세</q-btn>
+        <q-btn unelevated class="q-mr-xs">아파트,오피스텔,재개발...</q-btn>
+        <q-btn unelevated class="q-mr-xs">아파트,오피스텔,재개발...</q-btn>
       </div>
     </q-card-section>
   </q-card>
@@ -25,7 +27,7 @@
 export default {};
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
 .q-btn {
   background: #ffffff;
   /* Line/Gray_20 */
@@ -40,5 +42,14 @@ export default {};
   text-align: center;
   letter-spacing: -1.05px;
   color: #1a1a1a;
+}
+.scrolling-wrapper-flexbox {
+  display: flex;
+  flex-wrap: nowrap;
+  overflow-x: auto;
+
+  .q-btn {
+    flex: 0 0 auto;
+  }
 }
 </style>
