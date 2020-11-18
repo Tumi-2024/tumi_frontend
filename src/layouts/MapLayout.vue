1953cc248759e0c0a4d1b973617f7b37247b16d2
eevan7a9 <e7a9@protonmail.com>
2020-11-18 13:25:19 +0900

    created Layout for Map Sections

---
+++ src/layouts/MapLayout.vue
@@ -0,0 +1,21 @@
+<template>
+  <div style="min-height: 100vh" class="row column bg-positive">
+    <q-card flat square class="row full-width items-center">
+      <div>Toolbar here</div>
+    </q-card>
+
+    <q-card flat square class="q-pa-none bg-positive">
+      <router-view></router-view>
+    </q-card>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
