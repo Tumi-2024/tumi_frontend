41079a16bd52fc4cc211905a2987a89ddd529575
eevan7a9 <e7a9@protonmail.com>
2020-12-01 13:01:18 +0900

    perf(Map City): added bottom drawer

--- src/pages/MapCity/Index.vue
+++ src/pages/MapCity/Index.vue
@@ -1,15 +1,22 @@
 <template>
-  <div>
-    Map City Here
+  <div class="page-container full-height row justify-center items-center">
+    <h5 class="text-white">No Map :(</h5>
+    <bottom-drawer />
   </div>
 </template>
 
 <script>
+import { BottomDrawer } from "components/MapCityComponent";
 export default {
-
-}
+  components: {
+    "bottom-drawer": BottomDrawer
+  }
+};
 </script>
 
-<style>
-
+<style lang="scss" scoped>
+.page-container {
+  background: rgb(54, 54, 54);
+  min-height: calc(100vh - 118px);
+}
 </style>
