e8f24697b6414da400a4ce5cd7531adc3b33d0a9
eevan7a9 <e7a9@protonmail.com>
2020-12-02 11:47:10 +0900

    created(redevelopment-area: map section page

---
+++ src/pages/MapRedevelopmentArea/Index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <div class="page-container full-height column justify-center items-center">
+      <p class="text-white">Redevelopment Area</p>
+      <h5 class="text-white">No Map :(</h5>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+};
+</script>
+
+<style lang="scss" scoped>
+.page-container {
+  background: rgb(54, 54, 54);
+  min-height: calc(100vh - 118px);
+}
+</style>
