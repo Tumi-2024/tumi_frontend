c99549605e5b81c823e8405db82680aa39a1231d
eevan7a9 <e7a9@protonmail.com>
2020-12-02 12:05:00 +0900

    perf(MapRedevelopArea) : toolbar filter & bottom drawer

--- src/pages/MapRedevelopmentArea/Index.vue
+++ src/pages/MapRedevelopmentArea/Index.vue
@@ -1,14 +1,21 @@
 <template>
   <div>
+    <toolbar-filter></toolbar-filter>
     <div class="page-container full-height column justify-center items-center">
       <p class="text-white">Redevelopment Area</p>
       <h5 class="text-white">No Map :(</h5>
     </div>
+    <bottom-drawer></bottom-drawer>
   </div>
 </template>
 
 <script>
+import { ToolbarFilter, BottomDrawer } from "components/MapRedevelopmentComponents";
 export default {
+  components: {
+    "toolbar-filter": ToolbarFilter,
+    "bottom-drawer": BottomDrawer,
+  }
 };
 </script>
 
