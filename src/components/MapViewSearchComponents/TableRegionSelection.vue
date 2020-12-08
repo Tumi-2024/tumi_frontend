065c32c2f7a3e6851c91f88fefa78dbef7dd20f5
eevan7a9 <e7a9@protonmail.com>
2020-12-09 06:19:36 +0900

    feat: created TableRegionSelection Comp

---
+++ src/components/MapViewSearchComponents/TableRegionSelection.vue
@@ -0,0 +1,15 @@
+<template>
+  <q-card class="my-card" flat>
+    <q-card-section class="q-px-md">
+      <div>
+        Table
+      </div>
+    </q-card-section>
+  </q-card>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped></style>
