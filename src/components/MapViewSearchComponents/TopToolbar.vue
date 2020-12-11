bf52b12a6b5e165db1aebfb2b473c65039181de6
eevan7a9 <e7a9@protonmail.com>
2020-12-11 10:12:08 +0900

    perf(TopToolbar): added css style

--- src/components/MapViewSearchComponents/TopToolbar.vue
+++ src/components/MapViewSearchComponents/TopToolbar.vue
@@ -20,4 +20,12 @@
 export default {};
 </script>
 
-<style></style>
+<style scoped>
+.toolbar {
+  font-weight: 500;
+  font-size: 17px;
+  line-height: 44px;
+  letter-spacing: -1.275px;
+  color: #1a1a1a;
+}
+</style>
