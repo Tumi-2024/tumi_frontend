9c4dc5d6798413acc6b65e40f0e0242399ffd40c
eevan7a9 <e7a9@protonmail.com>
2020-12-30 21:14:34 +0900

    perf(GoogleMap): able to add addGeoJson

--- src/components/MapCityComponent/GoogleMap.vue
+++ src/components/MapCityComponent/GoogleMap.vue
@@ -3,7 +3,7 @@
     <GmapMap
       ref="mapRef"
       :center="{ lat: 37.5326, lng: 127.024612 }"
-      :zoom="9"
+      :zoom="12"
       :style="`height: ${mapSize.height}; width: ${mapSize.width};`"
     >
     </GmapMap>
@@ -12,7 +12,7 @@
 
 <script>
 import { gmapApi } from "gmap-vue";
-
+import { tumiSections } from "./tumi-sections-geojson.js";
 export default {
   data() {
     return {
@@ -31,13 +31,27 @@ export default {
     // we access the map Object
     this.$refs.mapRef.$mapPromise.then(map => {
       this.map = map;
-      this.map.panTo({ lat: 37.1326, lng: 127.024612 });
+      this.map.panTo({ lat: 37.5326, lng: 127.024612 });
 
       this.marker = new this.google.maps.Marker({
-        position: { lat: 37.1326, lng: 127.024612 },
+        position: { lat: 37.5326, lng: 127.024612 },
         map: this.map,
         title: "Hello World!"
       });
+      /**
+       *  we use loadGeoJson() for url
+       *  this.map.data.loadGeoJson("https:// url here /");
+       *
+       *  we use addGeoJson() for direct
+       *  this.map.data.addGeoJson({ object here })
+       */
+
+      this.map.data.addGeoJson(tumiSections);
+      this.map.data.setStyle({
+        fillColor: "green",
+        strokeWeight: 1
+      });
+      console.log(tumiSections);
     });
   },
 
