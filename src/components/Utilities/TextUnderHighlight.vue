147bc866bd5812756fad6c8e9c5b11ca331b8b53
eevan7a9 <e7a9@protonmail.com>
2021-01-09 00:28:33 +0900

    feat(TextUnderHighlight): new utitlity component

---
+++ src/components/Utilities/TextUnderHighlight.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="row" style="height: 24px">
+    <div class="text col-12 notosanskr-medium">{{ text }}</div>
+    <span class="highlighter full-width q-py-xs"></span>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    text: {
+      type: String,
+      default: ""
+    }
+  }
+};
+</script>
+
+<style scoped>
+.text {
+  z-index: 1;
+}
+.highlighter {
+  background: #ffd8cc;
+  margin-top: -10px;
+  z-index: 0;
+}
+</style>
