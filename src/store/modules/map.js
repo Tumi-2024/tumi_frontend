072cb60ad5cebd49f55777a22cf129be3e90b381
eevan7a9 <e7a9@protonmail.com>
2021-01-18 10:37:29 +0900

    perf(map): updated store

--- src/store/modules/map.js
+++ src/store/modules/map.js
@@ -1,20 +1,24 @@
+const initState = {
+  mode: "default",
+  mapZoom: 12,
+  mapCenter: {
+    lat: 37.5326,
+    lng: 127.024612
+  },
+  mapOptions: {
+    zoomControl: true,
+    mapTypeControl: false,
+    scaleControl: true,
+    streetViewControl: false,
+    rotateControl: false,
+    scrollwheel: true,
+    fullscreenControl: false,
+    disableDefaultUI: true
+  }
+}
 export const mapStore = {
   namespaced: true,
-  state: {
-    mode: "default",
-    mapZoom: 12,
-    mapCenter: { lat: 37.5326, lng: 127.024612 },
-    mapOptions: {
-      zoomControl: true,
-      mapTypeControl: false,
-      scaleControl: true,
-      streetViewControl: false,
-      rotateControl: false,
-      scrollwheel: true,
-      fullscreenControl: false,
-      disableDefaultUI: true
-    }
-  },
+  state: { ...initState },
   getters: {
     getMapMode: state => state.mode,
     getMapZoom: state => state.mapZoom,
@@ -29,21 +33,22 @@ export const mapStore = {
   },
   actions: {
     resetMap: context => {
-      context.commit("setMapMode", "default");
-      context.commit("setMapZoom", 12);
-      context.commit("setMapCenter", { lat: 37.5326, lng: 127.024612 });
-      context.commit("setMapOptions", {
-        zoomControl: true,
-        mapTypeControl: false,
-        scaleControl: true,
-        streetViewControl: false,
-        rotateControl: false,
-        scrollwheel: true,
-        fullscreenControl: false,
-        disableDefaultUI: true
-      });
+      context.commit("setMapMode", initState.mode);
+      context.commit("setMapZoom", initState.mapZoom);
+      context.commit("setMapCenter", initState.mapCenter);
+      context.commit("setMapOptions", initState.mapOptions);
     },
-    changeMapMode: (context, data) => context.commit("setMapMode", data),
+    /**
+     * Set the mode of the map
+     * @param context
+     * @param {('default'|'transaction'|'redevelop')} mode - options
+     */
+    changeMapMode: (context, mode = "default") => {
+      const possibleValues = ['default', 'transaction', 'redevelop']
+      if (possibleValues.includes(mode)) {
+        context.commit("setMapMode", mode);
+      }
+    },
     changeMapZoom: (context, data) => context.commit("setMapZoom", data),
     changeMapCenter: (context, data) => context.commit("setMapCenter", data),
     changeMapOptions: (context, data) => context.commit("setMapOptions", data)
