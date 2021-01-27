29a44dbe0b864d75238db3025fe1c036712183fe
eevan7a9 <e7a9@protonmail.com>
2021-01-27 22:20:13 +0900

    perf(RedevelopmentAreaInfo): header section

--- src/components/MapCityComponent/RedevelopmentAreaInfo/InformationFull.vue
+++ src/components/MapCityComponent/RedevelopmentAreaInfo/InformationFull.vue
@@ -1,6 +1,9 @@
 <template>
   <div>
-    content here
+    <div class="header-section notosanskr-medium">
+      재개발 구역정보
+      <q-icon name="close" size="24px" />
+    </div>
   </div>
 </template>
 
@@ -8,4 +11,17 @@
 export default {};
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.header-section {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 16px;
+
+  font-weight: 500;
+  font-size: 17px;
+  line-height: 44px;
+  letter-spacing: -1.275px;
+  color: #1a1a1a;
+}
+</style>
