0cec0f8dfb857727337428e1ce96b4fd64dc690e
eevan7a9 <e7a9@protonmail.com>
2021-02-02 19:57:58 +0900

    style(ShareToolbar): spacing

--- src/components/MapLayoutComponent/ShareToolBar.vue
+++ src/components/MapLayoutComponent/ShareToolBar.vue
@@ -1,27 +1,24 @@
 <template>
-  <q-card-section class="toolbar q-pa-none bg-white">
-    <div class="row q-ml-sm">
-      <q-btn flat padding="0px" :to="{ name: returnBtn.pathName }">
+  <q-card-section class="toolbar">
+    <q-btn flat padding="0px 8px 0 16px" class="row">
+      <div>
         <q-icon name="keyboard_backspace" size="24px" class="q-mr-sm"></q-icon>
-      </q-btn>
+      </div>
       <q-separator vertical inset />
-      <div class="q-ml-sm heading-nav-bar">동대문구 전농동</div>
-    </div>
+      <div class="heading-nav-bar q-ml-sm">동대문구 전농동</div>
+    </q-btn>
 
     <!-- Right side starts here -->
     <div class="flex-row">
-      <q-btn flat class="bg-white add-area">
-        <q-icon size="24px">
-            <img src="~assets/icons/like.svg" alt="" />
+      <q-btn flat padding="0 8px" class="bg-white add-area">
+        <q-icon size="44px">
+          <img src="~assets/icons/like.svg" alt="" />
         </q-icon>
       </q-btn>
 
-      <q-btn
-        flat
-        class="bg-white add-area"
-      >
-        <q-icon size="24px">
-            <img src="~assets/icons/share.svg" alt="" />
+      <q-btn flat padding="0 8px" class="bg-white add-area">
+        <q-icon size="44px">
+          <img src="~assets/icons/share.svg" alt="" />
         </q-icon>
       </q-btn>
     </div>
@@ -59,6 +56,8 @@ export default {
   display: flex;
   justify-content: space-between;
   align-items: center;
+  padding: 0;
+  background: white;
 }
 .add-area {
   font-weight: 500;
@@ -68,13 +67,12 @@ export default {
   letter-spacing: -1.2px;
   color: #1a1a1a;
 }
-.heading-nav-bar{
-  font-family: 'notosanskr-medium';
-  font-style: normal;
+.heading-nav-bar {
+  font-family: "notosanskr-medium";
   font-weight: 500;
   font-size: 17px;
   line-height: 44px;
   letter-spacing: -1.275px;
-  color: #1A1A1A;
+  color: #1a1a1a;
 }
 </style>
