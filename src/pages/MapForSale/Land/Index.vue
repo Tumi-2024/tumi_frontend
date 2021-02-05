d72c864c7a6d3ef619bd3cb905b42493e35afb7d
eevan7a9 <e7a9@protonmail.com>
2021-02-05 10:33:40 +0900

    style: remove unused code

--- src/pages/MapForSale/Land/Index.vue
+++ src/pages/MapForSale/Land/Index.vue
@@ -52,46 +52,7 @@ export default {
     "google-map": GoogleMap
   },
   data() {
-    return {
-      redevelopmentInformations: [
-        {
-          title: "추진위원회 승인",
-          date: "2004.07.20"
-        },
-        {
-          title: "정비구역지정",
-          date: "2005.05.19"
-        },
-        {
-          title: "조합설립인가",
-          date: "2005.05.19"
-        },
-        {
-          title: "시공사",
-          date: null
-        },
-        {
-          title: "사업시행인가",
-          date: "2005.05.19"
-        },
-        {
-          title: "관리처분인가",
-          date: "2005.05.19"
-        },
-        {
-          title: "착공",
-          date: "2005.05.19"
-        },
-        {
-          title: "일반분양",
-          date: null
-        },
-        {
-          title: "준공인가",
-          date: "2005.05.19"
-        }
-      ]
-    };
+    return {};
   }
 };
 </script>
