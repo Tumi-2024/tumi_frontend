ac65c51a70422af0b7d92194285647c80e62405c
eevan7a9 <e7a9@protonmail.com>
2021-02-20 16:50:54 +0900

    perf(AveragePriceHistory): average price & date

--- src/components/MapForSaleComponents/AveragePriceHistory.vue
+++ src/components/MapForSaleComponents/AveragePriceHistory.vue
@@ -1,5 +1,5 @@
 <template>
-  <q-card flat class="bg-white notosanskr-medium">
+  <q-card flat class="bg-white notosanskr-regular">
     <q-card-section class="row q-pb-none justify-between">
       <div class="title-heading col-6">
         최근 3년 평균 실거래가
@@ -16,6 +16,20 @@
         label-color="primary"
         v-if="areaOptions"
       />
+    </q-card-section>
+
+    <q-card-section class="q-pa-none">
+      <div class="average-pricing q-pa-md">
+        <div class="title q-py-xs">
+          최근 한달기준 실거래가 평균
+          <q-badge outline color="grey-13" class="q-mx-xs bg-white">
+            <span class="date  notosanskr-medium">2020년 06월</span>
+          </q-badge>
+        </div>
+        <div class="price-value notosanskr-medium">
+          11억 4,800만
+        </div>
+      </div>
     </q-card-section>
   </q-card>
 </template>
@@ -46,5 +60,29 @@ export default {
   border: #ff5a00 solid 2px;
   border-radius: 8px;
   padding-left: 10px;
+}
+.average-pricing {
+  border-radius: 8px;
+  background: #f7f7f7;
+  margin: 20px 16px;
+  .title {
+    font-size: 15px;
+    line-height: 22px;
+    letter-spacing: -1.125px;
+    color: #d32c00;
+    .date {
+      font-weight: 500;
+      font-size: 12px;
+      letter-spacing: -0.9px;
+      color: #707070;
+    }
+  }
+  .price-value {
+    font-weight: 500;
+    font-size: 21px;
+    line-height: 32px;
+    letter-spacing: -1.575px;
+    color: #1a1a1a;
+  }
 }
 </style>
