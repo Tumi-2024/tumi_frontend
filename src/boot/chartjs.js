f39d1a9cf10529c8f4b87720f68fb41eb31ed111
eevan7a9 <e7a9@protonmail.com>
2021-02-23 18:25:54 +0900

    perf(chartjs): set additional style for graph

--- src/boot/chartjs.js
+++ src/boot/chartjs.js
@@ -1,5 +1,9 @@
 import Chart from "chart.js";
 import "chartjs-plugin-datalabels";
-
+// we set the value if the barcharts at the top edge
 Chart.defaults.global.plugins.datalabels.anchor = "end";
 Chart.defaults.global.plugins.datalabels.align = "end";
+// we set additional space between the "legend" and the chart
+Chart.Legend.prototype.afterFit = function() {
+  this.height = this.height + 20;
+};
