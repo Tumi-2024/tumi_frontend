53d1c575e20fbe559d71ccef6605f9fbd6004a7c
eevan7a9 <e7a9@protonmail.com>
2021-02-25 14:41:10 +0900

    feat(MapCity): added map action buttons

---
+++ src/components/MapCityComponents/ActionButtons.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="action-container ">
+    <q-btn color="white" padding="8px" v-if="showAll">
+      <q-icon size="24px">
+        <img src="~assets/icons/like.svg" alt="" srcset="" />
+      </q-icon>
+    </q-btn>
+
+    <q-btn :color="cone ? 'primary' : 'white'" padding="8px" v-if="showAll">
+      <div>
+        <q-icon size="24px">
+          <img src="~assets/icons/cone.svg" alt="" srcset="" />
+        </q-icon>
+        <div class="off text-dark notosanskr-bold">
+          {{ cone ? "ON" : "OFF" }}
+        </div>
+      </div>
+    </q-btn>
+
+    <q-btn color="white" padding="8px">
+      <q-icon size="24px">
+        <img src="~assets/icons/target.svg" alt="" srcset="" />
+      </q-icon>
+    </q-btn>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      cone: false
+    };
+  },
+  props: {
+    showAll: {
+      type: Boolean,
+      default: false
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.action-container {
+  position: fixed;
+  z-index: 1;
+  padding-top: 8px;
+
+  display: flex;
+  flex-direction: column;
+}
+.q-btn {
+  border-radius: 8px;
+  margin: 4px 16px;
+  .off {
+    font-weight: bold;
+    font-size: 11px;
+    line-height: 16px;
+    text-align: center;
+    letter-spacing: -0.825px;
+    color: #1a1a1a;
+  }
+}
+</style>
