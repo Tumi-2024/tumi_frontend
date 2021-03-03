02f22a04836553e08e4144ffe2b9f1a1539d934d
eevan7a9 <e7a9@protonmail.com>
2021-03-04 00:33:54 +0900

    delete: contacted & Interest component

--- src/components/PropertyContactedComponents/index.js
+++
@@ -1,5 +0,0 @@
-import ContactedMenuItems from "./ContactedMenuItems";
-import ContactedBtns from "./ContactedBtns";
-import ContactedListItems from "./ContactedListItems";
-
-export { ContactedMenuItems, ContactedBtns, ContactedListItems };
