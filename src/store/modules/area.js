9bb8edac49dfc183f93777a9cd89dfcf220d26bc
eevan7a9 <e7a9@protonmail.com>
2021-03-15 07:11:41 +0900

    fixf(MapCityArea): redevelopemnt steps show

--- src/store/modules/area.js
+++ src/store/modules/area.js
@@ -15,7 +15,9 @@ export const areaStore = {
   },
   mutations: {
     setMapAreas: (state, payload) => (state.areas = payload),
-    setMapSelectedArea: (state, payload) => (state.selectedArea = payload)
+    setMapSelectedArea: (state, payload) => (state.selectedArea = payload),
+    setRedevelopmentSteps: (state, payload) =>
+      (state.selectedArea.steps = payload)
   },
   actions: {
     fetchMapAreas: async context => {
@@ -34,6 +36,22 @@ export const areaStore = {
       // context.commit("setMapAreas", data);
     },
     changeMapSelectedArea: (context, area) =>
-      context.commit("setMapSelectedArea", area)
+      context.commit("setMapSelectedArea", area),
+    fetchRedevelopmentSteps: async (context, id) => {
+      try {
+        const result = await Vue.prototype.$axios.get(
+          `/redevelopment_areas/${id}/steps`,
+          {
+            timeout: 10000
+          }
+        );
+        // console.log(result.data);
+        context.commit("setRedevelopmentSteps", result.data);
+      } catch (error) {
+        // if CORS error we use dummy data
+        context.commit("setRedevelopmentSteps", []);
+        console.log(error, "error");
+      }
+    }
   }
 };
