ed77344040256637f4ea52e5cbb47a38685273a1
배우현 <bwh1224@gmail.com>
2021-06-03 23:45:32 +0900

    [HomeDashboardButtons] refactoring

--- src/components/HomeComponents/HomeDashboardButtons.vue
+++ src/components/HomeComponents/HomeDashboardButtons.vue
@@ -1,67 +1,20 @@
 <template>
-  <div class="row">
-    <div
-      class="col-6 q-pr-xs q-pb-xs flex justify-center items-center"
+  <div class="row flex justify-between" style="padding-bottom: 24px;">
+    <q-btn
+      flat
+      :key="btnIndex"
+      v-for="(btn, btnIndex) of btnGroup"
+      class="btn-dash-board full-width"
+      :style="{ backgroundColor: btn.backgroundColor }"
+      :class="{ 'q-mb-sm': btnIndex < 2 }"
+      style="flex: 0 0 calc(50% - 4px);"
+      @click="btn.func"
     >
-      <q-btn
-        flat
-        @click="toRedevelopmentArea()"
-        class="btn-dash-board full-width"
-        style="background-color: #3ECCB2"
-      >
-        <p class="btn-title1">재개발 구역</p>
-        <p class="btn-title2">
-          지도에서 쉽게 <br />
-          재개발 구역 찾기
-        </p>
-        <q-icon class="btn-icon" name="img:icons/home-page-icon/4btns/3.svg" />
-      </q-btn>
-    </div>
-    <div class="col-6 q-pl-xs q-pb-xs flex justify-center items-center">
-      <q-btn
-        flat
-        @click="toMapCity()"
-        class="btn-dash-board full-width"
-        style="background-color: #4B62DE"
-      >
-        <p class="btn-title1">추천 매물</p>
-        <p class="btn-title2">
-          지도에서 쉽게 <br />
-          매물 찾기
-        </p>
-        <q-icon class="btn-icon" name="img:icons/home-page-icon/4btns/1.svg" />
-      </q-btn>
-    </div>
-    <div class="col-6 q-pr-xs q-pt-xs flex justify-center items-center" style="padding-bottom: 24px">
-      <q-btn
-        flat
-        @click="$router.push({ name: 'map_list_sale' })"
-        class="btn-dash-board full-width"
-        style="background-color: #FF7D36"
-      >
-        <p class="btn-title1">매물 검색</p>
-        <p class="btn-title2">
-          이번주 <br />
-          추천급매
-        </p>
-        <q-icon class="btn-icon" name="img:icons/home-page-icon/4btns/2.svg" />
-      </q-btn>
-    </div>
-    
-    <div
-      class="col-6 q-pl-xs q-pt-xs flex justify-center items-center"
-      style="padding-bottom: 24px"
-    >
-      <q-btn
-        flat
-        :to="{ path: '/insights/부동산팁' }"
-        class="btn-dash-board full-width"
-        style="background-color: #5893F7"
-      >
-        <p class="btn-title1">부동산<br />인사이트</p>
-        <q-icon class="btn-icon" name="img:icons/home-page-icon/4btns/4.svg" />
-      </q-btn>
-    </div>
+      <!-- @click="btn.func()" -->
+      <p class="btn-title1" v-html="btn.text" />
+      <p class="btn-title2" v-html="btn.subText" />
+      <q-icon class="btn-icon" :name="btn.img" />
+    </q-btn>
   </div>
 </template>
 
@@ -72,7 +25,36 @@ const { Geolocation } = Plugins;
 export default {
   data() {
     return {
-      text: null
+      text: null,
+      btnGroup: [
+        {
+          backgroundColor: "#4B62DE",
+          text: "추천 매물",
+          subText: "지도에서 쉽게<br/>추천매물 찾기",
+          img: "img:icons/home-page-icon/4btns/1.svg",
+          func: this.toMapCity
+        },
+        {
+          backgroundColor: "#3ECCB2",
+          text: "재개발 구역",
+          subText: `지도에서 쉽게<br/>재개발구역과 실거래가 찾기`,
+          img: "img:icons/home-page-icon/4btns/3.svg",
+          func: this.toRedevelopmentArea
+        },
+        {
+          backgroundColor: "#FF7D36",
+          text: "매물 급매",
+          subText: "투미매물<br/>상세 검색하기",
+          img: "img:icons/home-page-icon/4btns/2.svg",
+          func: () => this.$router.push({ name: "map_list_sale" })
+        },
+        {
+          backgroundColor: "#5893F7",
+          text: "부동산<br />인사이트",
+          img: "img:icons/home-page-icon/4btns/4.svg",
+          func: () => this.$router.push("/insights/부동산팁")
+        }
+      ]
     };
   },
   methods: {
@@ -83,27 +65,31 @@ export default {
       "changeToolbarTitle"
     ]),
     toMapCity() {
-      Geolocation.getCurrentPosition({ enableHighAccuracy: true }).then(position => {
-        const { latitude: lat, longitude: lng } = position.coords;
-        // console.log("Current", lat, lng);
-        this.$store.dispatch("map/resetMap");
-        this.changeMapCenter({ lat, lng });
-        this.$router.push({ name: "map_city" });
-      }).catch(e => {
-        console.log(e, "error");
-      });
+      Geolocation.getCurrentPosition({ enableHighAccuracy: true })
+        .then(position => {
+          const { latitude: lat, longitude: lng } = position.coords;
+          // console.log("Current", lat, lng);
+          this.$store.dispatch("map/resetMap");
+          this.changeMapCenter({ lat, lng });
+          this.$router.push({ name: "map_city" });
+        })
+        .catch(e => {
+          console.log(e, "error");
+        });
     },
     toRedevelopmentArea() {
       this.$router.push({ name: "map_city_area" });
     },
     getCurrentPosition() {
-      Geolocation.getCurrentPosition({ enableHighAccuracy: true }).then(position => {
-        const { latitude: lat, longitude: lng } = position.coords;
-        // console.log("Current", lat, lng);
-        this.changeUserLocation({ lat, lng });
-      }).catch(e => {
-        console.log(e, "error");
-      });
+      Geolocation.getCurrentPosition({ enableHighAccuracy: true })
+        .then(position => {
+          const { latitude: lat, longitude: lng } = position.coords;
+          // console.log("Current", lat, lng);
+          this.changeUserLocation({ lat, lng });
+        })
+        .catch(e => {
+          console.log(e, "error");
+        });
     }
   }
 };
