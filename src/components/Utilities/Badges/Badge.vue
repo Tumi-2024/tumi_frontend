48bd7d607a2822a360c64d8c502a2761fdfc3c7a
배우현 <bwh1224@gmail.com>
2021-06-11 14:24:38 +0900

    [BadgeList] Create BadgeList and Badge Component

---
+++ src/components/Utilities/Badges/Badge.vue
@@ -0,0 +1,77 @@
+<template>
+  <q-badge :class="badgeClass" class="">
+    <q-icon v-if="icon">
+      <img :src="icon" alt="" srcset="" />
+    </q-icon>
+    <slot name="label">
+    {{value}}
+    </slot>
+  </q-badge>
+</template>
+<script>
+export default {
+  props: {
+    value: {
+      type: String,
+      required: true
+    },
+    icon: {
+      type: String
+    },
+    pyeong: {
+      type: Boolean
+    },
+    recommend: {
+      type: Boolean
+    },
+    redevelopment: {
+      type: Boolean
+    },
+    price: {
+      type: Boolean
+    },
+    date: {
+      type: Boolean
+    }
+
+  },
+  data() {
+    return {
+      badgeClass: ''
+    }
+  },
+  mounted() {
+    switch (true) {
+      case this.houseType:
+        this.badgeClass = 'text-white bg-primary'
+        break;
+      case this.pyeong:
+        this.badgeClass = 'text-white bg-green'
+        break;
+      case this.recommend:
+        this.badgeClass = 'text-primary bg-white'
+        break;
+      case this.redevelopment:
+        this.badgeClass = 're-develop bg-white'
+        break;
+      case this.price:
+        this.badgeClass = 'bg-blue'
+        break;
+      case this.date:
+        this.badgeClass = 'date text-white bg-black'
+        break;
+      default:
+        this.badgeClass = ''
+    }
+  }
+}
+</script>
+<style lang="scss">
+.q-badge {
+  font-weight: bold;
+  font-size: 12px;
+  line-height: 16px;
+  text-align: center;
+  letter-spacing: -0.9px;
+}
+</style>
---
+++ src/components/Utilities/Badges/BadgeList.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="row">
+    <template v-for="(badge, index) of tags" >
+      <Badge
+        :key="index"
+        v-if="badge.isShow"
+        v-bind="{icon:  badge.icon, value: badge.value, [badge.type]: true}"
+        :class="{'q-mr-sm': index !== tags.length -1}"
+      />
+    </template>
+  </div>
+</template>
+<script>
+import Badge from './Badge'
+export default {
+  components: { Badge },
+  props: {
+    tags: {
+      type: Array
+    }
+  }
+}
+</script>
