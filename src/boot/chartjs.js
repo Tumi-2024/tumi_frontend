23311bf215230eb4efc8cf171171a2a40f38d06b
배우현 <bwh1224@gmail.com>
2021-07-05 12:33:08 +0900

    changed

--- src/boot/chartjs.js
+++ src/boot/chartjs.js
@@ -1,9 +1,9 @@
-import Chart from "chart.js";
-import "chartjs-plugin-datalabels";
-// we set the value if the barcharts at the top edge
-Chart.defaults.global.plugins.datalabels.anchor = "end";
-Chart.defaults.global.plugins.datalabels.align = "end";
+// import Chart from "chart.js";
+// import "chartjs-plugin-datalabels";
+// // we set the value if the barcharts at the top edge
+// Chart.defaults.global.plugins.datalabels.anchor = "end";
+// Chart.defaults.global.plugins.datalabels.align = "end";
 // we set additional space between the "legend" and the chart
-Chart.Legend.prototype.afterFit = function() {
-  this.height = this.height + 20;
-};
+// Chart.Legend.prototype.afterFit = function() {
+//   this.height = this.height + 20;
+// };
