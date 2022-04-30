9bc6cd370f9df936f310611b1d6d17c9b2789d9c
bwh1224@gmail.com <bwh1224@gmail.com>
2022-04-30 15:11:50 +0900

    초기값 변경

--- src/store/modules/searchQuery.js
+++ src/store/modules/searchQuery.js
@@ -7,6 +7,12 @@ const initState = {
         label: "아파트",
         valueTransaction: "APARTMENT",
         valueHouse: "아파트"
+      },
+      {
+        icon: require("assets/iconsNew/11.png"),
+        label: "아파트",
+        valueTransaction: "APARTMENT",
+        valueHouse: "아파트"
       }
     ],
     prices: [
@@ -39,6 +45,48 @@ export const searchQueryStore = {
           label: "아파트",
           valueTransaction: "APARTMENT",
           valueHouse: "아파트"
+        },
+        {
+          icon: require("assets/iconsNew/12.png"),
+          label: "연립/다세대",
+          valueTransaction: "ALLIANCE",
+          valueHouse: "연립ￜ다세대"
+        },
+        {
+          icon: require("assets/iconsNew/13.png"),
+          label: "단독/다가구",
+          valueTransaction: "SINGLE",
+          valueHouse: "단독ￜ다가구"
+        },
+        {
+          icon: require("assets/iconsNew/14.png"),
+          label: "원룸/오피스텔",
+          valueTransaction: "OFFICETEL",
+          valueHouse: "오피스텔"
+        },
+        {
+          icon: require("assets/iconsNew/16.png"),
+          label: "상업업무용",
+          valueTransaction: "COMMERCIAL",
+          valueHouse: "상업ￜ업무용"
+        },
+        {
+          icon: require("assets/iconsNew/15.png"),
+          label: "토지",
+          valueTransaction: "LAND",
+          valueHouse: "토지"
+        },
+        {
+          icon: require("assets/iconsNew/17.png"),
+          label: "무허가 건축물",
+          valueTransaction: "noname01",
+          valueHouse: "무허가 건축물"
+        },
+        {
+          icon: require("assets/iconsNew/18.png"),
+          label: "입주권",
+          valueTransaction: "noname02",
+          valueHouse: "입주권"
         }
       ],
       prices: [
