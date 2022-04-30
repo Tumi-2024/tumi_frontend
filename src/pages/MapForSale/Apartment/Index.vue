d71e3c73c6c184d812baf89ac58f3eaddd461817
bwh1224@gmail.com <bwh1224@gmail.com>
2022-04-30 15:25:02 +0900

    소재지 동

--- src/pages/MapForSale/Apartment/Index.vue
+++ src/pages/MapForSale/Apartment/Index.vue
@@ -312,8 +312,8 @@ export default {
           label: "읍면동리",
           value: houseInfo2.location.title
         },
-        { label: "소재지(동 이하)", value: houseInfo.location_dong },
-        { label: "도로명 (로/길 이하)", value: houseInfo.location_road },
+        { label: "소재지(동 이하)", value: houseInfo2.location_dong },
+        { label: "도로명 (로/길 이하)", value: houseInfo2.location_road },
         { label: "역세권", value: houseInfo.description_station_area }
       ];
     },
