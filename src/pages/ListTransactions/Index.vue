99eeaab352ea97a3cf6db5db4531832c94d78716
bwh1224@gmail.com <bwh1224@gmail.com>
2022-06-20 00:41:35 +0900

    transaction 업데이트

--- src/pages/ListTransactions/Index.vue
+++ src/pages/ListTransactions/Index.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <AreaListTransactions :list="list" />
+    <AreaListTransactions />
     <floating-button />
   </div>
 </template>
@@ -26,63 +26,10 @@ export default {
   },
   methods: {
     ...mapActions("map", ["changeToolbarTitle"]),
-    onSearch(e) {
-      if (e.length === 0) {
-        return;
-      }
-      switch (this.tab) {
-        case "지역":
-          this.getLocations(e);
-          break;
-        case "정비사업":
-          this.getRedevelopment(e);
-          break;
-        case "건물/단지":
-          this.getTransactions(e);
-          break;
-        default:
-          this.getAllTransactions(e);
-      }
-    },
-    async getAllTransactions() {
-      const { data } = await Vue.prototype.$axios.get(`/transaction_groups/`);
-      this.list = data.results.map((item) => {
-        return {
-          ...item,
-          ...item.recent_transactions?.[item.types[0]]
-        };
-      });
-    },
-
-    async getTransactions(query) {
-      const { data } = await Vue.prototype.$axios.get(
-        `/transaction_groups/${this.$route.query.transactionid}/transactions/`,
-        {
-          params: query
-        }
-      );
-    },
-    async getRedevelopment(e) {
-      const { data } = await Vue.prototype.$axios.get(
-        `/redevelopment_areas/?search=${e}`
-      );
-    },
-    async getLocations(e) {
-      const { data } = await Vue.prototype.$axios.get(
-        `/locations/?search=${e}`
-      );
-    }
+    
   },
   async beforeMount() {
     this.changeToolbarTitle("실거래가");
-    if (this.$route.query?.transactionid) {
-      const { data } = await Vue.prototype.$axios.get(
-        `/transaction_groups/${this.$route.query.transactionid}`
-      );
-      this.list = data.results;
-    } else {
-      this.getAllTransactions();
-    }
   }
 };
 </script>
